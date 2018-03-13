c902c1481f611aff10864a57e33762bb1da9722b
phill2mj <phill2mj@gmail.com>
2018-03-13 00:06:32 -0400

    edited per review, cleaned up, and commented

--- src/app/containers/card-deck/card-deck.component.ts
+++ src/app/containers/card-deck/card-deck.component.ts
@@ -9,8 +9,7 @@ import { MonsterCard } from '../../models/card.model';
             <monster-card (click)="cardClick(c)"
                 [ngClass]="{
                     'flipped flip-to-front': c.facing === 'up',
-                    'send-to-back': c.facing === 'down',
-                    'shuffle': this.shuffleDeck === true
+                    'send-to-back': c.facing === 'down'
                 }"
                 [title]="c.title"
                 [order]="c.order"
@@ -26,13 +25,13 @@ export class CardDeckComponent implements OnInit{
 
     monsterCards: MonsterCard[] = [];
 
-    shuffleDeck: boolean = false;
+    shuffleTrigger: boolean = false;
 
     constructor() { }
 
     ngOnInit() {
 
-        // generate some goofy test cards
+        // generate some gloomy test cards
         for (let i = 0; i < 10; i++) {
             let ord = Math.floor(Math.random() * 100);
             this.monsterCards.push(new MonsterCard('Flame Demon - 1', ord, 'icon ' + i.toString(), 'Some card description ' + i.toString()))
@@ -42,45 +41,33 @@ export class CardDeckComponent implements OnInit{
 
         // add in a test shuffle card
         this.monsterCards[5].shuffle = true;
-
-        // console.log(this.monsterCards);
     }
 
     private cardClick(card) {
         if (card.shuffle === true) {
-            for (let i = 0; i < 10; i++) {
-                let ord = Math.floor(Math.random() * 100);
-                this.monsterCards[i].order = ord;
-                this.monsterCards[i].facing = 'down';
-            }
-            this.sortInOrder(this.monsterCards, 'order');
-            this.shuffleDeck = true;
+            // TODO: what is this trigger used for
+            this.shuffleTrigger = true;
+        }
+        if (card.facing === 'up') {
+            // flip the first card down
+            card.facing = 'down';
+            // wait while the card animates
             setTimeout( () => {
-                this.monsterCards[this.monsterCards.length - 1].facing = 'up';
-                this.shuffleDeck = false;
-            }, 600);
-            // console.log(this.monsterCards);
-        } else {
-            if (card.facing === 'up') {
-                card.facing = 'down';
-                setTimeout( () => {
-                    this.monsterCards.pop();
-                    this.monsterCards.push(card);
-                    this.monsterCards.unshift(this.monsterCards[this.monsterCards.length - 1]);
-                    this.monsterCards.pop();
-                    this.monsterCards[this.monsterCards.length - 1].facing = 'up';
-                }, 0);
-            } else {
-                card.facing = 'up';
+                // send the top card to the back
+                this.monsterCards.unshift(this.monsterCards[this.monsterCards.length - 1]);
                 this.monsterCards.pop();
-                this.monsterCards.push(card);
-            }
+                // flip the top card
+                this.monsterCards[this.monsterCards.length - 1].facing = 'up';
+            }, 0);
+        } else {
+            // the deck is new, just flip the top card
+            card.facing = 'up';
         }
     }
 
-    public sortInOrder(cards, order) {
+    public sortInOrder(cards, key) {
         return cards.sort(function(a, b) {
-            var x = a[order]; var y = b[order];
+            var x = a[key]; var y = b[key];
             return ((x < y) ? -1 : ((x > y) ? 1 : 0));
         });
     }
