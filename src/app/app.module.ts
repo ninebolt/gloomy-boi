16915a7e752e06f1d2f2436bc91457c6c0d3419d
Ash B <ashbhatnagar7@gmail.com>
2018-04-06 20:03:42 -0400

    Resetting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ActionBarComponent } from './containers/action-bar/action-bar.component';
-import { InitativeTrackerComponent } from './containers/initative-tracker/initative-tracker.component';
-import { CharacterInitativeComponent } from './components/character-initative/character-initative.component';
 import { DeckComponent } from './components/deck/deck.component';
 import { MonsterCardComponent } from './components/monster-card/monster-card.component';
 import { ActionButtonComponent } from './components/action-button/action-button.component';
 import { SearchDropdownComponent } from './components/search-dropdown/search-dropdown.component';
 import { HealthTrackerComponent } from './containers/health-tracker/health-tracker.component';
 import { MonsterHealthComponent } from './components/monster-health/monster-health.component';
-import { MonsterCaseComponent } from './containers/monster-case/monster-case.component';
-import { MonsterBandComponent } from './components/monster-band/monster-band.component';
 
 import { NameFilterPipe } from './pipes/name-filter.pipe';
 import { OrderByPipe } from './pipes/order-by.pipe';
 
 import { LineParserService } from './services/line-parser.service';
-import { CharacterService } from './services/character.service';
-import { ScenarioService } from './services/scenario.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     ActionBarComponent,
-    InitativeTrackerComponent,
-    MonsterCaseComponent,
-    MonsterBandComponent,
-    CharacterInitativeComponent,
     DeckComponent,
     MonsterCardComponent,
     ActionButtonComponent,
